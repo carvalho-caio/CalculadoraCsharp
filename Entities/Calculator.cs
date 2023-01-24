17623c4e55110b3748450224d6106f91c179a3d9
Caio Carvalho <two_Caio@live.com>
2023-01-23 22:21:30 -0300

    first iteration

---
+++ Entities/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.Dependencies
+{
+    internal class Calculator
+    {
+        public static double Plus(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Minus(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multi(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Div(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Calculadora.Dependencies;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Welcome to the Calculator.");
+            Console.WriteLine("");
+
+            string Exit = "n";
+
+            do
+            {
+                Console.Write("Type the first number: ");
+                double A = Double.Parse(Console.ReadLine());
+
+                Console.Write("Type the second number: ");
+                double B = Double.Parse(Console.ReadLine());
+
+                Console.WriteLine("");
+
+                Console.Write("Which opereration do you want to execute? ");
+                Console.Write("\nPlus (+)\nMinus(-)\nMultiplication(*)\nDivision(/)\n");
+                string Operation = Console.ReadLine();
+
+                if (Operation == "+")
+                {
+                    Console.WriteLine(Calculator.Plus(A, B));
+                }
+                else if (Operation == "-")
+                {
+                    Console.WriteLine(Calculator.Minus(A, B));
+                }
+                else if (Operation == "*")
+                {
+                    Console.WriteLine(Calculator.Multi(A, B));
+                }
+                else if (Operation == "/")
+                {
+                    Console.WriteLine(Calculator.Div(A, B));
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Syntax");
+                    break;
+                }
+
+                Console.WriteLine("Do you want to keep using? (y/n)");
+                Exit = Console.ReadLine();
+
+            } while (Exit != "n");
+        }
+    }
+}
